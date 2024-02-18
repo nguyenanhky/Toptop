4064e805ddeb688d9872cf4e382d99880653da91
nguyenanhky <chienky2016@gmail.com>
2024-02-18 10:08:44 +0700

    followingScreen horizantalPager

--- app/src/main/java/com/nhatvm/toptop/ui/following/FollowingScreen.kt
+++ app/src/main/java/com/nhatvm/toptop/ui/following/FollowingScreen.kt
@@ -1,16 +1,28 @@
 package com.nhatvm.toptop.ui.following
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -19,14 +31,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
+import androidx.compose.ui.util.lerp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.media3.common.MediaItem
@@ -37,17 +53,73 @@ import androidx.media3.exoplayer.ExoPlayer
 import com.nhatvm.toptop.R
 import com.nhatvm.toptop.designsystem.TopTopVideoPlayer
 import com.nhatvm.toptop.ui.theme.ToptopTheme
+import kotlin.math.absoluteValue
 
+@OptIn(ExperimentalFoundationApi::class)
 @UnstableApi
 @Composable
 fun FollowingScreen(
-    
-) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black)) {
-        CreatorCard(name = "Donal Trump", nickName = "@trumg", onFollow = { /*TODO*/ }) {
 
+) {
+
+    val pagerState = rememberPagerState()
+    val cardWith = (LocalConfiguration.current.screenWidthDp * 2 / 3) - 24
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(48.dp))
+        Text(
+            text = "Trending Creators",
+            style = MaterialTheme.typography.h4.copy(color = Color.White)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "Follow an account to see their latest video here.",
+            style = MaterialTheme.typography.body1.copy(color = Color.Gray)
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+        HorizontalPager(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(0.8f),
+            pageCount = 10,
+            state = pagerState,
+            pageSize = PageSize.Fixed(cardWith.dp),
+            pageSpacing = 12.dp,
+            contentPadding = PaddingValues(start = 24.dp)
+        ) { page ->
+            Card(
+                modifier = Modifier
+                    .width(cardWith.dp)
+                    .aspectRatio(0.7f)
+                    .graphicsLayer {
+                        val pageOffset = (
+                                (pagerState.currentPage - page) + pagerState
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+
+                        // We animate the alpha, between 50% and 100%
+                        scaleY = lerp(
+                            start = 0.7f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+                    .clip(RoundedCornerShape(16.dp))
+
+            ) {
+
+                CreatorCard(
+                    name = "Donal Trump $page",
+                    nickName = "@donalTrump$page",
+                    onFollow = { }) {
+
+                }
+            }
         }
     }
 }
@@ -133,7 +205,7 @@ fun CreatorCard(
             bottom.linkTo(tvName.top, margin = 8.dp)
         })
 
-        val uri = RawResourceDataSource.buildRawResourceUri(R.raw.khesanh)
+        val uri = RawResourceDataSource.buildRawResourceUri(R.raw.haytuhaobanthan)
         val mediaItem = MediaItem.fromUri(uri)
         videoPlayer.setMediaItem(mediaItem)
         SideEffect {
